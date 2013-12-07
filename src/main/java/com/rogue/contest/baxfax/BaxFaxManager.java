426563b05fdd3c305e02ffa7f75ec3489e4ae706
Spencer <Spencer@129.21.140.110>
2013-12-07 14:15:53 -0500

    Steal baxfax for mbaxter

--- src/main/java/com/rogue/contest/baxfax/BaxFaxManager.java
+++ src/main/java/com/rogue/contest/baxfax/BaxFaxManager.java
@@ -17,6 +17,17 @@
 package com.rogue.contest.baxfax;
 
 import com.rogue.contest.Contest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 /**
  *
@@ -27,9 +38,41 @@ import com.rogue.contest.Contest;
 public class BaxFaxManager {
     
     private final Contest plugin;
+    private final String baxfaxLocation = "https://raw.github.com/RoyalDev/RoyalBot/master/src/main/resources/config.yml";
+    //private final LinkedQueue<String> baxfax = new LinkedQueue<String>();
     
     public BaxFaxManager(Contest plugin) {
         this.plugin = plugin;
+        ReadableByteChannel rbc = null;
+        FileOutputStream fos = null;
+        File f = new File(this.plugin.getDataFolder(), "config.yml");
+        try {
+            URL call = new URL(this.baxfaxLocation);
+            rbc = Channels.newChannel(call.openStream());
+            fos = new FileOutputStream(f);
+            fos.getChannel().transferFrom(rbc, 0, Integer.MAX_VALUE);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(BaxFaxManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BaxFaxManager.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (rbc != null) {
+                    rbc.close();
+                }
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(BaxFaxManager.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        YamlConfiguration yaml = YamlConfiguration.loadConfiguration(f);
+        
+    }
+    
+    public String newBaxFax() {
+        return null;
     }
 
 }
